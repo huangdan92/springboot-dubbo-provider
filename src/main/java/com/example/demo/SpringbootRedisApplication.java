12a841e7b6bedce79e89fc9baba1f17809347e00
huangdan92 <huangdan921@qq.com>
2023-02-28 15:34:33 +0800

    d

--- src/main/java/com/example/demo/SpringbootRedisApplication.java
+++ src/main/java/com/example/demo/SpringbootRedisApplication.java
@@ -1,20 +1,6 @@
 package com.example.demo;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class SpringbootApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(SpringbootApplication.class, args);
-//    }
-//
-//}
-
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -22,15 +8,12 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-//@EnableCachingleAutoConfiguration
 public class SpringbootRedisApplication extends SpringBootServletInitializer {
-
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(SpringbootRedisApplication.class);
     }
-
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootRedisApplication.class, args);
